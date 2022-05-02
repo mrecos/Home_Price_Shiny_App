1b534270bfaa6e10c1e87ea148f69e0e43e4055b
Matt Harris <mr.ecos@gmail.com>
2022-05-02 12:42:33 -0400

    cosmetic: new predicitons button and DR colors

--- Housing_Prediction/app.R
+++ Housing_Prediction/app.R
@@ -7,6 +7,8 @@ library(jsonlite)
 library(dplyr)
 library(shinyBS)
 library(scales)
+library(shinyWidgets) # for pretty slide
+library(DataRobotColors)
 
 dat <- read.csv("Boulder_Housing_Student_4326.csv") %>% 
   # rename("Latitude" = Y, "Longitude" = X) %>% 
@@ -47,32 +49,32 @@ pred_exp <- POST(URL_predex,
 httr::content(pred_exp, simplifyVector=TRUE)
 #################
 
-get_pred <- function(Latitude,Longitude,qualityCodeDscr,carStorageSF,
-                     nbrBedRoom, TotalFinishedSF){
-  ## quick model to estimate mainfloor from finished
-  mainfloorSF_lm <- lm(mainfloorSF ~ TotalFinishedSF, data = dat)
-  mainfloorSF <- predict(mainfloorSF_lm,
-                         data.frame(TotalFinishedSF = TotalFinishedSF))
-  ####
-  predictions <- dat[1,] 
-  predictions$id  <- 1
-  predictions$Latitude <- Latitude
-  predictions$Longitude <- Longitude
-  predictions$qualityCodeDscr  <- qualityCodeDscr
-  predictions$carStorageSF <- carStorageSF
-  predictions$year <- 2020 
-  predictions$nbrBedRoom <- nbrBedRoom
-  predictions$mainfloorSF <- mainfloorSF # prediction
-  predictions$TotalFinishedSF <- TotalFinishedSF
-  response <- POST(URL, 
-                   body = rjson::toJSON(unname(split(predictions, 1:nrow(predictions)))),
-                   add_headers("datarobot-key" = DATAROBOT_KEY),
-                   httr::content_type_json(),
-                   authenticate(USERNAME, API_TOKEN, type = "basic")
-  )
-  data <- fromJSON(content(response, "text", encoding = "UTF-8"), simplifyVector=FALSE)$data[[1]]$prediction
-  return(data)
-}
+# get_pred <- function(Latitude,Longitude,qualityCodeDscr,carStorageSF,
+#                      nbrBedRoom, TotalFinishedSF){
+#   ## quick model to estimate mainfloor from finished
+#   mainfloorSF_lm <- lm(mainfloorSF ~ TotalFinishedSF, data = dat)
+#   mainfloorSF <- predict(mainfloorSF_lm,
+#                          data.frame(TotalFinishedSF = TotalFinishedSF))
+#   ####
+#   predictions <- dat[1,] 
+#   predictions$id  <- 1
+#   predictions$Latitude <- Latitude
+#   predictions$Longitude <- Longitude
+#   predictions$qualityCodeDscr  <- qualityCodeDscr
+#   predictions$carStorageSF <- carStorageSF
+#   predictions$year <- 2020 
+#   predictions$nbrBedRoom <- nbrBedRoom
+#   predictions$mainfloorSF <- mainfloorSF # prediction
+#   predictions$TotalFinishedSF <- TotalFinishedSF
+#   response <- POST(URL, 
+#                    body = rjson::toJSON(unname(split(predictions, 1:nrow(predictions)))),
+#                    add_headers("datarobot-key" = DATAROBOT_KEY),
+#                    httr::content_type_json(),
+#                    authenticate(USERNAME, API_TOKEN, type = "basic")
+#   )
+#   data <- fromJSON(content(response, "text", encoding = "UTF-8"), simplifyVector=FALSE)$data[[1]]$prediction
+#   return(data)
+# }
 get_pred_exp <- function(Latitude,Longitude,qualityCodeDscr,carStorageSF,
                      nbrBedRoom, TotalFinishedSF){
   ## quick model to estimate mainfloor from finished
@@ -109,13 +111,17 @@ get_pred_exp <- function(Latitude,Longitude,qualityCodeDscr,carStorageSF,
 ui = dashboardPage(
   dashboardHeader(title = "Home Price Demo"),
   dashboardSidebar(
-    sidebarMenu(menuItem(
-      "Home Prediction Menu",
-      tabName = "Dashboard",
-      icon = icon("table")
-    )),
-    checkboxInput("pred_mode", label = "Turn on Prediction Mode", value = FALSE),
-    # checkboxInput("pred_mode", label = "Turn on Prediction Explanations", value = FALSE),
+    # sidebarMenu(
+    #   menuItem(
+    #   "Home Prediction Menu",
+    #   tabName = "Dashboard",
+    #   icon = icon("table")
+    # )
+    # ),
+    switchInput("pred_mode", 
+                label = "Predictions", 
+                value = FALSE),
+    # checkboxInput("pred_mode", label = "Turn on Prediction Mode", value = FALSE),
     sliderInput("TotalFinishedSF", "Total Finished SqFt:",
                 min = 500, max = 5000, value = 2050
     ),
@@ -143,12 +149,19 @@ server = function(input, output, session) {
   
   pred <- reactiveValues(pred_price = NULL,
                          pred_exp = NULL)
-  etst <- reactiveValues(click = NA)
 
   map = createLeafletMap(session, 'map')
+  
   pal <- colorNumeric(
     palette = "PiYG",
     domain = c(50000,3000000))
+  
+  DR_colors <- as.character(DataRobot_get_palettes()$DR_Diverging)
+  pal_DR <- colorFactor(
+    palette = DR_colors,
+    domain = dat$price
+  )
+  
   session$onFlushed(once = TRUE, function() {
 
     pnt_labels <- paste("<b>Actual Price:</b>",
@@ -165,7 +178,7 @@ server = function(input, output, session) {
     output$map <- renderLeaflet({
       leaflet(dat) %>%
         addCircleMarkers(~ Longitude, ~ Latitude,
-                         color = ~pal(price),
+                         color = ~pal_DR(price),
                          label = lapply(pnt_labels, htmltools::HTML)) %>%
         setView(lat = 40.03494514594114,lng = -105.27329016435645,zoom = 11) %>%
         addProviderTiles("CartoDB.Positron",group = "CartoDB.Positron") %>%
